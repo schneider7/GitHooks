6642488b95e697958dda6b47b3c42444b35b5a78
Mike Schneider <mschneider3254@gmail.com>
2019-06-17 16:31:01 -0500

    Change name of route to index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 GitHooks::Engine.routes.draw do
-  get '/pullrequest',  to: "static_pages#git_hooks"
+  get '/pullrequest',  to: "static_pages#index"
   post '/git_hooks', to: "pullrequest#label"
 end
