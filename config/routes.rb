18e7fecdfa70206d1a9fbf50caa5ec88a4cd641c
Mike Schneider <mschneider3254@gmail.com>
2019-06-17 16:24:21 -0500

    Change route

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 GitHooks::Engine.routes.draw do
-  get '/git_hooks',  to: "static_pages#git_hooks"
+  get '/pullrequest',  to: "static_pages#git_hooks"
   post '/git_hooks', to: "pullrequest#label"
 end
