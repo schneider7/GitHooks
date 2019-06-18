b939f87e114a260e3fd46cc9144d6ca1a6ece6d3
Mike Schneider <mschneider3254@gmail.com>
2019-06-18 14:39:07 -0500

    Rollback changes

--- app/controllers/git_hooks/pullrequests_controller.rb
+++ app/controllers/git_hooks/pullrequests_controller.rb
@@ -1,22 +1,19 @@
 module GitHooks
   class PullrequestsController < ::GitHooks::ApplicationController
     
-    require 'octokit'
-    client = Octokit::Client.new(GithubWebhooks.access_token)
-
-    def label
+      def label
       request.body.rewind
       request_payload = JSON.parse(request.body.read)
       
       action_done = request_payload["action"]
       issue_number = request_payload["pull_request"]["number"]
 
-      if action_done == "submitted"
-        client.remove_label('https://github.com/michael-schneider3/sample_app', issue_number, 'QA Review')
-        client.remove_label('https://github.com/michael-schneider3/sample_app', issue_number, 'Dev Review')
+      if action_done == "labeled"
+        HTTPable.remove_label(issue_number, 'QA Review')
+        HTTPable.remove_label(issue_number, 'Dev Review')
       end
       
-status 200
+      status 200 
     end
   end
 end
