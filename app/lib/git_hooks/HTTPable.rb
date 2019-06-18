29d3161946f45d834a99d8ab3ef661c04aff5556
Mike Schneider <mschneider3254@gmail.com>
2019-06-18 16:53:39 -0500

    Fix typo

--- app/lib/git_hooks/HTTPable.rb
+++ app/lib/git_hooks/HTTPable.rb
@@ -29,12 +29,14 @@ module GitHooks
 
       response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
         http.request(request)
+      end
 
       github_response = JSON.parse(http.request(request).body)
 
       File.open('/mnt/c/ruby/GitHooks_logs.txt') do |f|
         f.write(github_response) 
       end
+      
     end
   end
 end
