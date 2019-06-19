8b9a7fbe09a7ae2423b5db7851e1e743034bd81f
Mike Schneider <mschneider3254@gmail.com>
2019-06-19 12:15:21 -0500

    Rremove self from method def

--- app/controllers/git_hooks/pullrequests_controller.rb
+++ app/controllers/git_hooks/pullrequests_controller.rb
@@ -1,7 +1,7 @@
 module GitHooks
   class PullrequestsController < ApplicationController
   
-    def self.label
+    def label
       request.body.rewind
       request_payload = JSON.parse(request.body.read)
       
