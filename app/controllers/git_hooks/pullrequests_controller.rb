4bd16a229666a9adea7ae08be2f43fd8857ba744
Mike Schneider <mschneider3254@gmail.com>
2019-06-20 13:59:27 -0500

    Tweak actions again

--- app/controllers/git_hooks/pullrequests_controller.rb
+++ app/controllers/git_hooks/pullrequests_controller.rb
@@ -7,12 +7,11 @@ module GitHooks
       request_payload = JSON.parse(request.body.read)
       
       action_done = request_payload["action"]
-      number = request_payload["pull_request"]["number"]
+      number      = request_payload["pull_request"]["number"]
 
       if action_done == "labeled"
-        res = Http.remove_label(number, 'QA Review')
-        Rails.logger.debug JSON.parse(res.body).inspect
-        Http.remove_label(number, 'bug')
+        Http.remove_label(number,  'QA Review')
+        Http.remove_label(number, 'Dev Review')
       end
       
       head :ok 
