3aef1a10acbf00d440022014c97339f3b447092c
Mike Schneider <mschneider3254@gmail.com>
2019-06-21 12:28:17 -0500

    New method add_label, updated actions in pullrequests_controller

--- app/controllers/git_hooks/pullrequests_controller.rb
+++ app/controllers/git_hooks/pullrequests_controller.rb
@@ -8,10 +8,16 @@ module GitHooks
       
       action_done = request_payload["action"]
       number      = request_payload["pull_request"]["number"]
+      status      = request_payload["review"]["state"]
 
-      if action_done == "submitted"
-        Http.remove_label(number,  'QA Review')
-        Http.remove_label(number, 'Dev Review')
+      if action_done == "submitted" && status == "approved"
+        Http.remove_label(number, "Dev Review")
+        Http.add_label(number, ["Dev Approved"])
+        Http.add_label(number, ["QA Review"])        
+      end
+
+      if action_done == "submitted" && status == "changes_requested"
+        Http.remove_label(number, "Dev Review")
       end
       
       head :ok 
--- app/lib/git_hooks/http.rb
+++ app/lib/git_hooks/http.rb
@@ -10,16 +10,21 @@ module GitHooks
     end
     
     def self.remove_label(issue_number, label_name)
-      # encoded_label_name = URI.encode(label_name)
       options = githooks_defaults("/#{issue_number}/labels/#{label_name}")
       http_request('Delete', options)
     end
+
+    def self.add_label(issue_number, labels) #takes an array as an argument
+      options = githooks_defaults("/#{issue_number}/labels")
+      http_request('Post', options.merge(params: { 'labels': labels }))
 
     def self.http_request(method, options={})
       encoded_uri = URI.encode(options[:defaults][:uri])
       uri = URI.parse(encoded_uri)
       request = Net::HTTP::const_get(method).new(uri)
       request["Authorization"] = "token #{options[:defaults][:token]}"
+
+      request.body = JSON.dump(options[:params]) unless options[:params].nil?
 
       response = Net::HTTP.start(uri.hostname, uri.port, { use_ssl: uri.scheme == "https" }) do |http|
         http.request(request)
