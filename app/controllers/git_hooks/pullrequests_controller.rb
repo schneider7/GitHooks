59c0153672655e6ade5820b4c728f4177dccb58c
Mike Schneider <mschneider3254@gmail.com>
2019-06-24 14:41:56 -0500

    Remove test action, clean up code

--- app/controllers/git_hooks/pullrequests_controller.rb
+++ app/controllers/git_hooks/pullrequests_controller.rb
@@ -7,12 +7,9 @@ module GitHooks
       request_payload = JSON.parse(request.body.read)
       
       # Get values from the parsed JSON that we'll need as arguments later on
-      repo_modified    = request_payload["pull_request"]["head"]["repo"]["name"]
       action_done      = request_payload["action"]
       number           = request_payload["pull_request"]["number"]
-
-      labels_present = Http.get_labels(repo_modified, number)
-      labels_present.map! { |h| h['name'] }
+      repo_modified    = request_payload["pull_request"]["head"]["repo"]["name"]
       
       if GitHooks.active_repos.include?(repo_modified)
       
@@ -23,12 +20,14 @@ module GitHooks
         # If review request is approved on an active repo
         if action_done == "submitted" && submitted_status == "approved"
         
+          labels_present = Http.get_labels(repo_modified, number)
+          labels_present.map! { |h| h['name'] }
+
           if labels_present.include?("Dev Review")
             Http.remove_label(repo_modified, number, "Dev Review")
           end 
           
-          Http.add_label(repo_modified, number, ["Dev Approved"]) 
-          Http.add_label(repo_modified, number, ["QA Review"])        
+          Http.add_label(repo_modified, number, ["Dev Approved", "QA Review"])
         end
       
         # If review request is 'declined'
@@ -36,17 +35,12 @@ module GitHooks
           Http.remove_label(repo_modified, number, "Dev Review")
         end
       
-        # Test action
-        if action_done == "labeled" || action_done == "unlabeled"
-          Http.add_comment(repo_modified, number, labels_present*",")
-          Http.add_label(repo_modified, number, ["WOOHOO"])
-        end
-
         head :ok 
 
-      else Rails.logger.error "That repo is not in your list of active repos.
-        Add it in the config file of your app. See 
-        the README on GitHub for more info."
+      else Rails.logger.error
+         "GITHOOKS ERROR: That repo is not in your list of 
+         active repos. Add it in the config file of your app. 
+         See the README on GitHub for more info."
 
       end
     end
