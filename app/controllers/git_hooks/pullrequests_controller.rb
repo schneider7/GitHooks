1ebb7aef8c2fbeb57d7c97a6d985308f354dbe56
Mike Schneider <mschneider3254@gmail.com>
2019-06-24 10:56:31 -0500

    Wrap everything in an if statement, so that the action only occurs if repo in active repos

--- app/controllers/git_hooks/pullrequests_controller.rb
+++ app/controllers/git_hooks/pullrequests_controller.rb
@@ -8,32 +8,39 @@ module GitHooks
       
       # Get values from the parsed JSON that we'll need as arguments later on
       repo_modified    = request_payload["pull_request"]["head"]["repo"]["name"]
-      action_done      = request_payload["action"]
-      number           = request_payload["pull_request"]["number"]
-      
-      if action_done == "submitted"
-        submitted_status = request_payload["review"]["state"]
-      end
+      number = request_payload["pull_request"]["number"]
 
-      # If review request is approved on an active repo
-      if action_done == "submitted" && submitted_status == "approved"
-        Http.remove_label(repo_modified, number, "Dev Review")
-        Http.add_label(repo_modified, number, ["Dev Approved"]) 
-        Http.add_label(repo_modified, number, ["QA Review"])        
-      end
+      if GitHooks.active_repos.include?(repo_modified)
       
-      # If review request is 'declined'
-      if action_done == "submitted" && submitted_status == "changes_requested" 
-        Http.remove_label(repo_modified, number, "Dev Review")
-      end
+        if action_done == "submitted"
+          submitted_status = request_payload["review"]["state"]
+        end
+
+        # If review request is approved on an active repo
+        if action_done == "submitted" && submitted_status == "approved"
+          Http.remove_label(repo_modified, number, "Dev Review")
+          Http.add_label(repo_modified, number, ["Dev Approved"]) 
+          Http.add_label(repo_modified, number, ["QA Review"])        
+        end
+      
+        # If review request is 'declined'
+        if action_done == "submitted" && submitted_status == "changes_requested" 
+          Http.remove_label(repo_modified, number, "Dev Review")
+        end
       
 
-      # Test action
-      if (action_done == "labeled" || action_done == "unlabeled") && GitHooks.active_repos.include?(repo_modified)
-        Http.add_label(repo_modified, number, ["WOOHOO"])
-      end
+        # Test action
+        if (action_done == "labeled" || action_done == "unlabeled")
+          Http.add_label(repo_modified, number, ["WOOHOO"])
+        end
 
-      head :ok 
+        head :ok 
+
+      else Rails.logger.error "That repo is not in your list of active repos.
+        Add it in the config file of your app. See 
+        the README on GitHub for more info."
+
+      end
     end
   end
 end
