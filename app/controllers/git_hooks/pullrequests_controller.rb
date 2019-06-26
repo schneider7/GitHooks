5317564b9d6caa866847daa45c1de656e72e5e1a
Mike Schneider <mschneider3254@gmail.com>
2019-06-26 15:26:55 -0500

    Rewrite to remove 1 step in config

--- app/controllers/git_hooks/pullrequests_controller.rb
+++ app/controllers/git_hooks/pullrequests_controller.rb
@@ -9,42 +9,43 @@ module GitHooks
       action_done      = request_payload['action']
       number           = request_payload['pull_request']['number']
       repo_modified    = request_payload['pull_request']['head']['repo']['name']
+      repo_full_name   = request_payload['pull_request']['head']['repo']['full_name'] # Includes org name in string
       
       if GitHooks.active_repos.include?(repo_modified) && action_done == 'submitted'
-        labels_present = Http.get_labels(repo_modified, number)
+        labels_present = Http.get_labels(repo_full_name, number)
         labels_present.map! { |h| h['name'] }
         submitted_status = request_payload['review']['state']
 
         if submitted_status == 'approved'
           
           unless GitHooks.approved[:add].blank?
-            Http.add_label(repo_modified, number, GitHooks.approved[:add])
+            Http.add_label(repo_full_name, number, GitHooks.approved[:add])
           end
   
           GitHooks.approved[:remove]&.each do |label| 
             if labels_present.include?(label)
-              Http.remove_label(repo_modified, number, label)
+              Http.remove_label(repo_full_name, number, label)
             end
           end
 
           unless GitHooks.approved[:comment].blank?
-            Http.add_comment(repo_modified, number, GitHooks.approved[:comment])
+            Http.add_comment(repo_full_name, number, GitHooks.approved[:comment])
           end
           
         elsif submitted_status == 'changes_requested'
           
           unless GitHooks.rejected[:add].blank?
-            Http.add_label(repo_modified, number, GitHooks.rejected[:add])
+            Http.add_label(repo_full_name, number, GitHooks.rejected[:add])
           end
 
           GitHooks.rejected[:remove]&.each do |label|
             if labels_present.include?(label)
-              Http.remove_label(repo_modified, number, label)
+              Http.remove_label(repo_full_name, number, label)
             end
           end
 
           unless GitHooks.rejected[:comment].blank?
-            Http.add_comment(repo_modified, number, GitHooks.rejected[:comment])
+            Http.add_comment(repo_full_name, number, GitHooks.rejected[:comment])
           end
 
         end
--- app/lib/git_hooks/http.rb
+++ app/lib/git_hooks/http.rb
@@ -3,28 +3,30 @@ require 'net/http'
 module GitHooks
   module Http
     
+    base_uri = "https://api.github.com/repos"
+
     def self.githooks_defaults(path)
-      uri = ENV["BASE_URI"] + path
+      uri = base_uri + path
       {defaults: {uri: uri, token: ENV["GITHUB_TOKEN"]}}
     end
     
-    def self.remove_label(repo_name, issue_number, label_name)
-      options = githooks_defaults("/#{repo_name}/issues/#{issue_number}/labels/#{label_name}")
+    def self.remove_label(full_name, issue_number, label_name)
+      options = githooks_defaults("/#{full_name}/issues/#{issue_number}/labels/#{label_name}")
       http_request('Delete', options)
     end
 
-    def self.add_label(repo_name, issue_number, labels)
-      options = githooks_defaults("/#{repo_name}/issues/#{issue_number}/labels")
+    def self.add_label(full_name, issue_number, labels)
+      options = githooks_defaults("/#{full_name}/issues/#{issue_number}/labels")
       http_request('Post', options.merge(params: { 'labels': labels }))
     end
 
-    def self.get_labels(repo_name, issue_number)
-      options = githooks_defaults("/#{repo_name}/issues/#{issue_number}/labels")
+    def self.get_labels(full_name, issue_number)
+      options = githooks_defaults("/#{full_name}/issues/#{issue_number}/labels")
       http_request('Get', options)
     end
 
-    def self.add_comment(repo_name, issue_number, body)
-      options = githooks_defaults("/#{repo_name}/issues/#{issue_number}/comments")
+    def self.add_comment(full_name, issue_number, body)
+      options = githooks_defaults("/#{full_name}/issues/#{issue_number}/comments")
       http_request('Post', options.merge(params: { body: body }))
     end
 
