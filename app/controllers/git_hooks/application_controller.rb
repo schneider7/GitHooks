11c718141ddd3d3a68271b8be7c892798e039b0c
Mike Schneider <mschneider3254@gmail.com>
2019-06-27 11:33:56 -0500

    Change protect from forgery type

--- app/controllers/git_hooks/application_controller.rb
+++ app/controllers/git_hooks/application_controller.rb
@@ -1,5 +1,5 @@
 module GitHooks
   class ApplicationController < ActionController::Base
-    skip_before_action :verify_authenticity_token 
+    protect_from_forgery unless: -> { request.format.json? } 
   end
 end
